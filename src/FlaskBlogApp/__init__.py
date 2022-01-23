a4e59364c6c5e0765460cc751e4f7cf4588a86ce
Alexandros Petridis <al1998petridis@gmail.com>
2022-01-23 23:40:56 +0200

    database change

--- src/FlaskBlogApp/__init__.py
+++ src/FlaskBlogApp/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'b668cbc68d29fd2b7f5976c54c39f6ec'
 app.config['WTF_CSRF_SECRET_KEY'] = 'fe9d487ba2c9a1f13a5d72fa0d76d3fb'
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_course_database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://eeryzobhhwloel:6a3e78a4b0ea9342444e1e034a42c93e59f9ba3370119efc3c105f5b5fa26455@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/d5iv4ffbk9qomn"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
